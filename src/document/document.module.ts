0cd7142eeb1e4522cad7285ea3049badcb457f6b
kwadwoatta <princeofori065@gmail.com>
2024-04-05 12:47:38 +0000

    module(document): inject upload module into document module

--- src/document/document.module.ts
+++ src/document/document.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UploadModule } from 'src/upload/upload.module';
+import { UploadService } from 'src/upload/upload.service';
 import { DocumentController } from './document.controller';
 import { DocumentService } from './document.service';
 
 @Module({
-  providers: [DocumentService],
+  imports: [UploadModule],
+  providers: [DocumentService, UploadService],
   controllers: [DocumentController],
 })
 export class DocumentModule {}
